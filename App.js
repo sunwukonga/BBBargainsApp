24464e7e2030214765180d300be1348911495f6f
Paul Desmond Parker <pauldesmondparker@gmail.com>
2018-02-21 12:38:20 +0800

    Add items to .gitignore

--- App.js
+++ App.js
@@ -16,7 +16,8 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Shake or press menu button for dev menu\n' +
+    'Better yet, type ./shake.sh in terminal then allow live reload on phone',
 });
 
 type Props = {};
